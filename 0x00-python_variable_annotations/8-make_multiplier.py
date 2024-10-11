cde285069aa1c82b193b68c58fdec98e22c8093d
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-11 17:30:29 +0000

    Multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -9,7 +9,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiply(value: float) -> float:
+        """Return that return func"""
         return multiplier * value
-
     """Return sum of two float"""
     return multiply
