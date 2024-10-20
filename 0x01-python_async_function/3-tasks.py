caa155caded160450273c6b38ff93666c52c9b10
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-20 10:30:07 +0000

    wait random

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,7 +7,7 @@ This module contain task_wait_randome function
 import asyncio
 
 
-wait_n = __import__("0-basic_async_stntax").wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
